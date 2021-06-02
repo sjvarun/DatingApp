57833d7e3cd38139380dc4e83f9a6b31a3394d33
varun.s <varun.s@sonata-software.com>
2021-06-02 11:55:12 +0530

    end of section 2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public UsersController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public IEnumerable<AppUser> GetUsers()
+        {
+            return _dataContext.Users.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public AppUser GetUser(int id)
+        {
+            return _dataContext.Users.Find(id);
+        }
+    }
+}
